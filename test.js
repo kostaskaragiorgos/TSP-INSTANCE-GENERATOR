9987cf43b6abb80e4c973c4ec7f59c8d48026e2e
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-05-24 12:30:51 +0300

    check text

---
+++ test.js
@@ -0,0 +1,9 @@
+function getInputValue(){
+    var inputVal = document.getElementById("numberofr").value;
+    if (inputVal > 3){
+        alert("OK")
+    }else{
+        alert("NOT OK")
+
+    }
+}
