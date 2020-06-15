dc7626702efac4862f1ae2a3e77d8d85daeb8aeb
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-06-15 21:48:40 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 function getInputValue(){
-    var inputVal = document.getElementById("numberofr").value;
-    if (inputVal > 4){
-        // TO DO
+    var inputVal = Number(document.getElementById("numberofr").value);
+    if (inputVal > 4 && Number.isInteger(inputVal)){
+        alert(inputVal);
     }else{
         alert("ERROR ADD AN INTEGER NUMBRER >4")
 
