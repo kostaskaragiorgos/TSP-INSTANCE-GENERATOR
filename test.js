0bc8e3c5b87d657013447190d9bac8aa416b8487
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-06-16 11:41:16 +0300

    update

--- test.js
+++ test.js
@@ -1,9 +1,43 @@
 function getInputValue(){
     var inputVal = Number(document.getElementById("numberofr").value);
+    var fromVal = Number(document.getElementById("distancefrom").value);
+    var toVal = Number(document.getElementById("distanceto").value);
     if (inputVal > 4 && Number.isInteger(inputVal)){
-        alert(inputVal);
+        if (fromVal>0 && Number.isInteger(fromVal)){
+            if (toVal>fromVal && Number.isInteger(toVal)){
+                alert("Nodes"+inputVal+"F"+fromVal+"TO"+toVal);
+                makearry(inputVal,fromVal,toVal);
+            }
+            else{
+                alert("ERROR DISTANCE TO VALUE MUST BE > FROM");
+            }
+        }
+        else{
+            alert("ERROR DISTANCE FROM VALUE MUST BE > 0");
+        }
     }else{
-        alert("ERROR ADD AN INTEGER NUMBRER >4")
+        alert("ERROR ADD AN INTEGER NUMBRER >4");
 
     }
 }
+
+let getRandomNumber = function(start, range){
+    let getRandom = Math.floor((Math.random()* range)+ start);
+    while (getRandom > range){
+            getRandom = Math.floor((Math.random()* range)+ start);
+    }
+    return getRandom;
+    }
+
+function makearry(inputVal,fromVal,toVal){
+    for (var i =0; i<inputVal;i++)
+    {
+        for (var j=0;j<inputVal;j++)
+        {
+            console.log(getRandomNumber(fromVal,toVal));
+        }
+    }
+}
+
+
+    
