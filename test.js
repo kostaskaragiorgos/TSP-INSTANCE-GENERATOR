8336a42337e1f4fa7e8fd6aa9fb522102b2fea43
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-06-28 15:55:24 +0300

    profinal

--- test.js
+++ test.js
@@ -2,11 +2,22 @@ function getInputValue(){
     var inputVal = Number(document.getElementById("numberofr").value);
     var fromVal = Number(document.getElementById("distancefrom").value);
     var toVal = Number(document.getElementById("distanceto").value);
+    var radiob = document.getElementById("Radiob").value;
     if (inputVal > 4 && Number.isInteger(inputVal)){
         if (fromVal>0 && Number.isInteger(fromVal)){
             if (toVal>fromVal && Number.isInteger(toVal)){
-                alert("Nodes"+inputVal+"F"+fromVal+"TO"+toVal);
-                makearry(inputVal,fromVal,toVal);
+                if (radiob == "Asymmetric"){
+                    arr= asymetric(inputVal, fromVal, toVal);
+                    console.log(arr);
+                    
+                }
+                else if(radiob=="Symmetric"){
+                   arr=  symetric(inputVal, fromVal, toVal);
+                   console.log(arr);
+                }
+                downloadtofile(arr,'testf.txt','application/octet-stream');
+
+               
             }
             else{
                 alert("ERROR DISTANCE TO VALUE MUST BE > FROM");
@@ -21,23 +32,65 @@ function getInputValue(){
     }
 }
 
+
 let getRandomNumber = function(start, range){
     let getRandom = Math.floor((Math.random()* range)+ start);
     while (getRandom > range){
-            getRandom = Math.floor((Math.random()* range)+ start);
+        getRandom = Math.floor((Math.random()* range)+ start);
     }
     return getRandom;
-    }
-
-function makearry(inputVal,fromVal,toVal){
-    for (var i =0; i<inputVal;i++)
-    {
-        for (var j=0;j<inputVal;j++)
-        {
-            console.log(getRandomNumber(fromVal,toVal));
-        }
-    }
 }
 
 
+let symetric = function(inputv, fromVal, toVal){
+    var arr= [];
+    for (var i=0;i<inputv;i++){
+        arr[i]=[];
+        for (var j=0;j<inputv;j++){
+             if (i>j){
+                arr[i][j]= getRandomNumber(fromVal,toVal);
+            }
+            else if(i==j){
+                arr[i][j]=0;
+
+            } 
+        }
+    }
+    for (var i=0;i<inputv;i++){
+        for (var j=0;j<inputv;j++){
+            if (i<j){
+                arr[i][j]= arr[j][i];
+            }
+        }
+    }
+    return arr;
+}
+
+let asymetric = function(inputv, fromVal , toVal){
+    var arr = [new Array(inputv)];
     
+    for (var i =0; i<inputv;i++){
+        arr[i]=[];
+        for (var j=0;j<inputv;j++)
+        {
+            if (i != j){
+            arr[i][j] = getRandomNumber(fromVal,toVal);
+            }
+            else if(i==j){
+                arr[i][j]=0;
+            }
+        }
+    }
+    return arr;
+}
+
+const downloadtofile = (content, filename,contentType) => {
+    const a = document.createElement('a');
+
+    const file = new Blob([content],{type: contentType});
+
+	a.href= URL.createObjectURL(file);
+	a.download= filename;
+	a.click();
+	URL.revokeObjectURL(a.href);
+}
